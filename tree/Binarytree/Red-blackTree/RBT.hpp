713b11d4eb4073f435fc94a882f1a17100b779e7
Anass Mounadi <mounadi2015@gmail.com>
2023-03-28 06:46:06 +0000

    add erase and operator=

--- tree/Binarytree/Red-blackTree/RBT.hpp
+++ tree/Binarytree/Red-blackTree/RBT.hpp
@@ -2,6 +2,8 @@
 #define RBT_HPP
 
 #include <iostream>
+#include <exception>
+
 template <class k, class v>
 class rbt
 {
@@ -14,20 +16,82 @@ class rbt
             k _key;
             v _value;
             Node(k key, v value) : parent(NULL), left(NULL), right(NULL),
-                                isBlack(false), isLeftChild(false), _key(key), _value(value) {}
+            isBlack(false), isLeftChild(false), _key(key), _value(value) {}
         };
         Node *_root;
         size_t _size;
+        Node * findMin(Node *root)
+        {
+            while(root && root->left)
+                root = root->left;
+            return root;
+        }
+        Node * deleteNode(Node *root, const k &key)
+        {
+            if(root == NULL) return root;
+            else if (root->_key > key) root->left = deleteNode(root->left,key);
+            else if (root->_key < key) root->right=  deleteNode(root->right,key);            
+            else
+            {
+                // One child
+                if (!root->right && !root->left)
+                {    
+                    delete root;
+                    root = NULL;
+                    return root;
+                }
+                else if(!root->left) // One child in right
+                {
+                    Node *tmp = root;
+                    root = root->right;
+                    delete tmp;
+                    return root;
+                }
+                else if (!root->right) // One child in left
+                {
+                    Node *tmp = root;
+                    root = root->left;
+                    delete tmp;
+                    return root;
+                }
+                else
+                {
+                    Node *tmp = findMin(root->right);
+                    root->_value = tmp->_value;
+                    root->_key = tmp->_key;
+                    deleteNode(root->right,tmp->_key);
+                }
+            }
+            return root;
+        }
+        void copyNode(Node *root) 
+        {
+            if (root == NULL)
+                return ;
+            copyNode(root->left);
+            insert(root->_key,root->_value);
+            copyNode(root->right);
+        }
     public:
-        explicit rbt() : _root(NULL), _size(0) {}
+        rbt() : _root(NULL), _size(0) {}
+        explicit rbt(const rbt & other):_root(NULL),_size(0)
+        {
+            this->_size = other._size;
+            if (other._root != NULL) copyNode(other._root);
+        }
+        rbt & operator=(const rbt & other)
+        {
+            this->~rbt();
+            new(this)rbt(other);
+            return *this;
+        }
         ~rbt()
         {
-            if (_root != nullptr) {
+            if (_root != NULL) {
                 deleteTree(_root);
-                _root = nullptr;
+                _root = NULL;
             }
         }
-
         void deleteTree(Node* node) 
         {
             if (node == nullptr)
@@ -42,7 +106,7 @@ class rbt
         void insert(k key, v value)
         {
             Node *newNode = new Node(key, value);
-            newNode->isBlack = false;
+            newNode->isBlack = false; // New node is always colored red
 
             Node *parent = NULL;
             Node *current = _root;
@@ -61,10 +125,10 @@ class rbt
                 }
             }
             newNode->parent = parent;
-            if (parent == nullptr)
+            if (parent == NULL) // Tree was empty, new node becomes root
             {
                 _root = newNode;       
-                _root->isBlack = true; 
+                _root->isBlack = true; // Root node must be colored black
             }
             else if (key < parent->_key) 
                 parent->left = newNode;
@@ -72,13 +136,14 @@ class rbt
                 parent->right = newNode;
             _size++;
         }
+        void erase(const k& key){_root = deleteNode(_root,key);}
         v & operator[] (const k& key)
         {
             Node *root = _root; 
             while(root)
             {
                 if (root->_key== key)
-                    break;
+                    return root->_value ;              
                 {
                     if (key < root->_key)
                         root = root->left;
@@ -86,11 +151,21 @@ class rbt
                         root = root->right;
                 }
             }
+            throw std::runtime_error("error : not found");
             return root->_value;
         }
-        k key() { return _root->_key; }
-        v value() { return _root->_value; }
+        void traversal(){traversal(_root);}
+        void traversal(Node *root) 
+        {
+            if (root == NULL)
+                return ;
+            traversal(root->left);
+            std::cout << " " << root->_value << std::endl;;
+            traversal(root->right);
+        }
+        k &key() { return _root->_key; }
+        v &value() { return _root->_value; }
         size_t size() { return _size; }
+        bool empty(){ return _root == NULL; }
 };
-
 #endif
